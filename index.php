f7755470847908cd43c006ba1df14389e8788c73
Drake Scott <dascott0204@gmail.com>
2017-01-20 03:57:57 -0600

    sanitize inputs and put php inside html

--- index.php
+++ index.php
@@ -1,9 +1,46 @@
-<?php
+<!DOCTYPE html>
 
-    $greeting = 'Hello';
+<html lang="en">
 
-    $name = 'Drake Scott';
+<head>
 
-    echo "{$greeting}, {$name}";
+    <meta charset="utf-8" />
 
- ?>
+    <title>Document</title>
+
+    <style>
+        header {
+            background: #e3e3e3;
+
+            padding: 2em;
+
+            text-align: center;
+        }
+    </style>
+
+</head>
+
+<body>
+
+
+    <header>
+        <h1>
+            <?php
+
+            $name = $_GET['name'];
+
+             echo "Hello, $name"; ?>
+        </h1>
+
+        <h1>
+
+            <?php echo "Hello, ". htmlspecialchars($_GET['name']);
+            /* (<?=) is the same as <?php echo
+                sanitize inputs by htmlspecialchars and other stuff
+            */?>
+        </h1>
+    </header>
+
+</body>
+
+</html>
