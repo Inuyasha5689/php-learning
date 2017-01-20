d631247d766a8bad868a37c1e2e54009e5082161
Drake Scott <dascott0204@gmail.com>
2017-01-20 04:03:25 -0600

    serparation of concerns

--- index.php
+++ index.php
@@ -1,46 +1,7 @@
-<!DOCTYPE html>
+<?php
 
-<html lang="en">
+    $greeting = 'Hello, World';
 
-<head>
+require 'index.view.php';
 
-    <meta charset="utf-8" />
-
-    <title>Document</title>
-
-    <style>
-        header {
-            background: #e3e3e3;
-
-            padding: 2em;
-
-            text-align: center;
-        }
-    </style>
-
-</head>
-
-<body>
-
-
-    <header>
-        <h1>
-            <?php
-
-            $name = $_GET['name'];
-
-             echo "Hello, $name"; ?>
-        </h1>
-
-        <h1>
-
-            <?php echo "Hello, ". htmlspecialchars($_GET['name']);
-            /* (<?=) is the same as <?php echo
-                sanitize inputs by htmlspecialchars and other stuff
-            */?>
-        </h1>
-    </header>
-
-</body>
-
-</html>
+ ?>
